cc4af745a9d5308a020776845e4b86e4e2cd5760
binaya121 <binayashah30@gmail.com>
2023-09-14 01:07:20 +1200

    Add project files.

---
+++ gradecalculator/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gradecalculator
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public double MathsMarks { get; set; }
+        public double PhysicsMarks { get; set; }
+        public double ChemistryMarks { get; set; }
+        public double ComputerScienceMarks { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public double CalculateAverage()
+        {
+            return (MathsMarks + PhysicsMarks + ChemistryMarks + ComputerScienceMarks) / 4.0;
+        }
+
+        public char CalculateGrade()
+        {
+            double average = CalculateAverage();
+            if (average >= 80)
+                return 'A';
+            else if (average >= 70)
+                return 'B';
+            else if (average >= 60)
+                return 'C';
+            else if (average >= 50)
+                return 'D';
+            else
+                return 'F';
+        }
+    }
+}
+
+
---
+++ gradecalculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gradecalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student's name:");
+            string name = Console.ReadLine();
+
+            Student student = new Student(name);
+
+            try
+            {
+                Console.Write("Enter marks for Maths (out of 100): ");
+                student.MathsMarks = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter marks for Physics (out of 100): ");
+                student.PhysicsMarks = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter marks for Chemistry (out of 100): ");
+                student.ChemistryMarks = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter marks for Computer Science (out of 100): ");
+                student.ComputerScienceMarks = Convert.ToDouble(Console.ReadLine());
+
+                char grade = student.CalculateGrade();
+
+                switch (grade)
+                {
+                    case 'A':
+                        Console.WriteLine("Excellent! Your grade is A");
+                        break;
+                    case 'B':
+                        Console.WriteLine("Good! Your grade is B");
+                        break;
+                    case 'C':
+                        Console.WriteLine("Satisfactory. Your grade is C");
+                        break;
+                    case 'D':
+                        Console.WriteLine("Pass. Your grade is D");
+                        break;
+                    case 'F':
+                        Console.WriteLine("Fail. Your grade is F");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid grade");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter numeric values for marks.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            Console.ReadKey();  
+
+        }
+    }
+}
